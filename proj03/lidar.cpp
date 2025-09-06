84b1b93759e351ec16c62928db9743ec58caec3d
Cesar Grossl <cgrossl00@gmail.com>
2025-09-06 11:38:11 -0300

    fix: ajustes

--- proj03/lidar.cpp
+++ proj03/lidar.cpp
@@ -1,50 +1,45 @@
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
-#include <termios.h>
-#include <linux/serial.h>
+#include <string.h>
 #include <sys/ioctl.h>
-#include <cstring>
-#include <cstdint>
+#include <linux/termios.h> // Para struct termios2
+#include <asm/ioctls.h>    // Para TCGETS2 / TCSETS2
+#include <asm/termbits.h>  // Para BOTHER
 
 #define LIDAR_SERIAL "/dev/ttyUSB0"
-#define LIDAR_BAUD 256000
+#define BAUDRATE 256000
 
-int abrir_serial(const char* device, int baudrate) {
+int abrir_serial(const char* device) {
     int fd = open(device, O_RDWR | O_NOCTTY | O_SYNC);
     if (fd < 0) {
-        perror("Erro ao abrir serial");
+        perror("Erro ao abrir porta serial");
         return -1;
     }
 
-    struct termios2 tio2;
-    if (ioctl(fd, TCGETS2, &tio2) < 0) {
-        perror("Erro ao obter termios2");
+    struct termios2 tio;
+    if (ioctl(fd, TCGETS2, &tio) < 0) {
+        perror("Erro TCGETS2");
         close(fd);
         return -1;
     }
 
-    tio2.c_cflag &= ~CBAUD;
-    tio2.c_cflag |= BOTHER;
-    tio2.c_ispeed = baudrate;
-    tio2.c_ospeed = baudrate;
+    tio.c_cflag &= ~CBAUD;
+    tio.c_cflag |= BOTHER;
+    tio.c_ispeed = BAUDRATE;
+    tio.c_ospeed = BAUDRATE;
+    tio.c_cflag |= (CLOCAL | CREAD | CS8);
+    tio.c_cflag &= ~(PARENB | CSTOPB | CRTSCTS);
 
-    tio2.c_cflag |= (CLOCAL | CREAD);
-    tio2.c_cflag &= ~CSIZE;
-    tio2.c_cflag |= CS8;
-    tio2.c_cflag &= ~PARENB;
-    tio2.c_cflag &= ~CSTOPB;
-    tio2.c_cflag &= ~CRTSCTS;
+    tio.c_iflag = IGNPAR;
+    tio.c_oflag = 0;
+    tio.c_lflag = 0;
 
-    tio2.c_iflag = IGNPAR;
-    tio2.c_oflag = 0;
-    tio2.c_lflag = 0;
+    tio.c_cc[VTIME] = 1;
+    tio.c_cc[VMIN] = 0;
 
-    tio2.c_cc[VTIME] = 1;
-    tio2.c_cc[VMIN] = 0;
-
-    if (ioctl(fd, TCSETS2, &tio2) < 0) {
-        perror("Erro ao aplicar termios2");
+    if (ioctl(fd, TCSETS2, &tio) < 0) {
+        perror("Erro TCSETS2");
         close(fd);
         return -1;
     }
@@ -53,33 +48,26 @@ int abrir_serial(const char* device, int baudrate) {
 }
 
 void enviar_comando(int fd, uint8_t cmd1, uint8_t cmd2) {
-    uint8_t comando[2] = {cmd1, cmd2};
-    write(fd, comando, 2);
+    uint8_t cmd[] = {cmd1, cmd2};
+    write(fd, cmd, 2);
 }
 
-void ler_resposta(int fd, int bytes) {
+void ler_resposta(int fd, int tamanho) {
     uint8_t buffer[128] = {0};
-    int lidos = read(fd, buffer, bytes);
-    std::cout << "Bytes lidos: " << lidos << std::endl;
-
+    int lidos = read(fd, buffer, tamanho);
+    std::cout << "Lidos: " << lidos << " bytes: ";
     for (int i = 0; i < lidos; ++i)
         printf("%02X ", buffer[i]);
-
     std::cout << std::endl;
 }
 
 int main() {
-    int serial_fd = abrir_serial(LIDAR_SERIAL, LIDAR_BAUD);
-    if (serial_fd < 0) {
-        return 1;
-    }
+    int fd = abrir_serial(LIDAR_SERIAL);
+    if (fd < 0) return 1;
 
-    // Comando GET_INFO: 0xA5 0x50
-    enviar_comando(serial_fd, 0xA5, 0x50);
-    usleep(50000); // Aguarda resposta
-
-    ler_resposta(serial_fd, 32);
-
-    close(serial_fd);
+    enviar_comando(fd, 0xA5, 0x50); // GET_INFO
+    usleep(50000);
+    ler_resposta(fd, 32);
+    close(fd);
     return 0;
 }
