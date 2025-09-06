b750e0317f6ec643872472c5c67727bd3f903e8b
Cesar Grossl <cgrossl00@gmail.com>
2025-09-06 11:36:30 -0300

    fix: alterar

--- proj03/lidar.cpp
+++ proj03/lidar.cpp
@@ -1,115 +1,84 @@
-// Controle de motores com L298N e desvio de obstáculos usando RPLIDAR C1
-// Sem ROS, apenas C++ puro para Raspberry Pi
-
 #include <iostream>
-#include <fstream>
-#include <unistd.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <termios.h>
-#include <vector>
-#include <cmath>
+#include <linux/serial.h>
+#include <sys/ioctl.h>
+#include <cstring>
+#include <cstdint>
 
-#define IN1_GPIO "/sys/class/gpio/gpio17/value"  // GPIO17 -> IN1
-#define IN2_GPIO "/sys/class/gpio/gpio27/value"  // GPIO27 -> IN2
-#define IN3_GPIO "/sys/class/gpio/gpio22/value"  // GPIO22 -> IN3
-#define IN4_GPIO "/sys/class/gpio/gpio23/value"  // GPIO23 -> IN4
 #define LIDAR_SERIAL "/dev/ttyUSB0"
-
-void write_gpio(const std::string &path, const std::string &value) {
-    std::ofstream gpio(path);
-    if (gpio.is_open()) {
-        gpio << value;
-        gpio.close();
-    }
-}
-
-void frente() {
-    write_gpio(IN1_GPIO, "1");
-    write_gpio(IN2_GPIO, "0");
-    write_gpio(IN3_GPIO, "1");
-    write_gpio(IN4_GPIO, "0");
-}
-
-void parar() {
-    write_gpio(IN1_GPIO, "0");
-    write_gpio(IN2_GPIO, "0");
-    write_gpio(IN3_GPIO, "0");
-    write_gpio(IN4_GPIO, "0");
-}
-
-void girar_direita() {
-    write_gpio(IN1_GPIO, "1");
-    write_gpio(IN2_GPIO, "0");
-    write_gpio(IN3_GPIO, "0");
-    write_gpio(IN4_GPIO, "1");
-}
+#define LIDAR_BAUD 256000
 
 int abrir_serial(const char* device, int baudrate) {
     int fd = open(device, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0) return -1;
+    if (fd < 0) {
+        perror("Erro ao abrir serial");
+        return -1;
+    }
 
-    struct termios tty;
-    tcgetattr(fd, &tty);
-    cfsetospeed(&tty, baudrate);
-    cfsetispeed(&tty, baudrate);
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
-    tty.c_iflag &= ~IGNBRK;
-    tty.c_lflag = 0;
-    tty.c_oflag = 0;
-    tty.c_cc[VMIN] = 0;
-    tty.c_cc[VTIME] = 1;
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-    tty.c_cflag |= (CLOCAL | CREAD);
-    tty.c_cflag &= ~(PARENB | PARODD);
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CRTSCTS;
+    struct termios2 tio2;
+    if (ioctl(fd, TCGETS2, &tio2) < 0) {
+        perror("Erro ao obter termios2");
+        close(fd);
+        return -1;
+    }
 
-    tcsetattr(fd, TCSANOW, &tty);
+    tio2.c_cflag &= ~CBAUD;
+    tio2.c_cflag |= BOTHER;
+    tio2.c_ispeed = baudrate;
+    tio2.c_ospeed = baudrate;
+
+    tio2.c_cflag |= (CLOCAL | CREAD);
+    tio2.c_cflag &= ~CSIZE;
+    tio2.c_cflag |= CS8;
+    tio2.c_cflag &= ~PARENB;
+    tio2.c_cflag &= ~CSTOPB;
+    tio2.c_cflag &= ~CRTSCTS;
+
+    tio2.c_iflag = IGNPAR;
+    tio2.c_oflag = 0;
+    tio2.c_lflag = 0;
+
+    tio2.c_cc[VTIME] = 1;
+    tio2.c_cc[VMIN] = 0;
+
+    if (ioctl(fd, TCSETS2, &tio2) < 0) {
+        perror("Erro ao aplicar termios2");
+        close(fd);
+        return -1;
+    }
+
     return fd;
 }
 
-bool detectar_obstaculo(int serial_fd) {
-    // Comando Start Scan: 0xA5 0x20
-    unsigned char start_scan[] = {0xA5, 0x20};
-    write(serial_fd, start_scan, 2);
-    usleep(200000);
+void enviar_comando(int fd, uint8_t cmd1, uint8_t cmd2) {
+    uint8_t comando[2] = {cmd1, cmd2};
+    write(fd, comando, 2);
+}
 
-    unsigned char buffer[5];
-    read(serial_fd, buffer, 5);  // resposta de 5 bytes
+void ler_resposta(int fd, int bytes) {
+    uint8_t buffer[128] = {0};
+    int lidos = read(fd, buffer, bytes);
+    std::cout << "Bytes lidos: " << lidos << std::endl;
 
-    unsigned char sample[47];
-    int n = read(serial_fd, sample, 47);
-    if (n < 5) return false;
+    for (int i = 0; i < lidos; ++i)
+        printf("%02X ", buffer[i]);
 
-    for (int i = 0; i < n - 4; i += 5) {
-        int distance = ((sample[i+3] << 8) | sample[i+2]) / 4;  // mm
-        float angle = ((sample[i+1] >> 1) + ((sample[i+2] & 0x01) << 7)) * 0.25;
-        if (distance > 100 && distance < 300 && angle > 60 && angle < 120) {
-            return true;  // obstáculo entre 60° e 120° a menos de 30cm
-        }
-    }
-    return false;
+    std::cout << std::endl;
 }
 
 int main() {
-    int serial_fd = abrir_serial(LIDAR_SERIAL, B256000);
+    int serial_fd = abrir_serial(LIDAR_SERIAL, LIDAR_BAUD);
     if (serial_fd < 0) {
-        std::cerr << "Erro ao abrir LIDAR" << std::endl;
         return 1;
     }
 
-    while (true) {
-        if (detectar_obstaculo(serial_fd)) {
-            parar();
-            usleep(500000);
-            girar_direita();
-            usleep(700000);
-            parar();
-        } else {
-            frente();
-        }
-        usleep(100000);
-    }
+    // Comando GET_INFO: 0xA5 0x50
+    enviar_comando(serial_fd, 0xA5, 0x50);
+    usleep(50000); // Aguarda resposta
+
+    ler_resposta(serial_fd, 32);
 
     close(serial_fd);
     return 0;
